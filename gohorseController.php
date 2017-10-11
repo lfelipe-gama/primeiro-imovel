44f7763d402c5083832c0a256ac0998ffcfebeb7
Felipe <lfelipe@websis.inf.br>
2017-10-11 00:44:00 -0300

    first deploy

---
+++ gohorseController.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+/**
+ * Created by PhpStorm.
+ * User: Felipe
+ * Date: 09/10/2017
+ * Time: 16:24
+ *
+ *
+ * GO HORSE SCRIPT MVP
+ *
+ */
+
+include_once ("mysql_crud.php");
+
+$db = new Database();
+$db->connect();
+
+if (isset($_SESSION['step'])) {
+    $_SESSION['step']++;
+} else {
+    $_SESSION['step'] = 1;
+}
+
+if (isset($_POST)) {
+    $_SESSION = array_merge($_SESSION, $_POST);
+
+    if ($_SESSION['step'] > 15) {
+
+        unset($_SESSION['step']);
+
+        $db->insert('leads',$_SESSION);
+        $res = $db->getResult();
+
+        //var_dump($res);
+        header('Location: obrigado.html');
+        exit();
+    }
+
+        header('Location: step' . $_SESSION['step'] . '.html');
+
+}
