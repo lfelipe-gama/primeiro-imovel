5814ea0c870775e798fb5ffbd0a9e886277898a0
Felipe <lfelipe@websis.inf.br>
2017-10-13 03:28:18 -0300

    phpmailer envia email thanks page

--- gohorseController.php
+++ gohorseController.php
@@ -32,7 +32,69 @@ if (isset($_POST)) {
         session_destroy();
 
         //var_dump($res);
-        header('Location: obrigado.html');
+
+    include "PHPMailer-master/PHPMailerAutoload.php";
+
+
+    $mail = new PHPMailer();
+
+    $mail->IsSMTP();
+    $mail->Host = "smtp.umbler.com";
+    $mail->Port = 587;
+
+    $mail->SMTPAuth = true;
+    $mail->Username = 'marcus@primeiro-imovel.com';
+    $mail->Password = 'cuzao123';
+
+// Configurações de compatibilidade para autenticação em TLS
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => true,
+            'allow_self_signed' => true,
+        )
+    );
+
+    $mail->SMTPDebug = 2; // Você pode habilitar esta opção caso tenha problemas. Assim pode identificar mensagens de erro.
+
+    $mail->From = "marcus@primeiro-imovel.com"; // Seu e-mail
+    $mail->FromName = "[Primeiro Imóvel] Seu primeiro imóvel, nosso primeiro passo!"; // Seu nome
+
+// Define o(s) destinatário(s)
+    $mail->AddAddress( $_SESSION['email'], $_SESSION['nome']);
+
+// BCC - Cópia oculta
+    $mail->AddBCC('lipe.smo@gmail.com', 'Felipe');
+
+// Definir se o e-mail é em formato HTML ou texto plano
+    $mail->IsHTML(true); // Formato HTML . Use "false" para enviar em formato texto simples.
+
+    $mail->CharSet = 'UTF-8'; // Charset (opcional)
+
+// Assunto da mensagem
+    $mail->Subject = "Orçamento Festa - ".$_POST['person'];
+
+// Corpo do email
+    $mail->Body = '<b>SOLICITAÇÃO DE ORÇAMENTO VIA SITE</b>
+                    <br><br>
+                    <b>Nome: </b>';
+
+// Envia o e-mail
+    $enviado = $mail->Send();
+
+// Exibe uma mensagem de resultado
+    if ($enviado) {
+        $retorno = array('codigo' => 1, 'mensagem' => '<br>Em breve entraremos em contato!');
+        echo json_encode($retorno);
+        exit();
+    } else {
+        $retorno = array('codigo' => 0, 'mensagem' => $mail->ErrorInfo);
+        echo json_encode($retorno);
+        exit();
+    }
+
+
+        //header('Location: obrigado.html');
         exit();
     }
 
