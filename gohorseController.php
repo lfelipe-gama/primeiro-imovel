e5681f6f0944c46dccd70db949cfa8d8ca34b543
Felipe <lfelipe@websis.inf.br>
2017-10-13 03:45:24 -0300

    envia email thaks page - conteudo

--- gohorseController.php
+++ gohorseController.php
@@ -11,7 +11,7 @@ session_start();
  *
  */
 
-include_once ("mysql_crud.php");
+include_once("mysql_crud.php");
 $db = new Database();
 $db->connect();
 
@@ -24,79 +24,76 @@ if (isset($_POST)) {
 
     if ($pg > 12) {
 
-        $_SESSION['interesse'] = implode(",",$_SESSION['interesse']);
+        $_SESSION['interesse'] = implode(",", $_SESSION['interesse']);
 
-        $db->insert('leads',$_SESSION);
+        $db->insert('leads', $_SESSION);
         $res = $db->getResult();
 
-    include "PHPMailer-master/PHPMailerAutoload.php";
+        include "PHPMailer-master/PHPMailerAutoload.php";
 
 
-    $mail = new PHPMailer();
+        $mail = new PHPMailer();
 
-    $mail->IsSMTP();
-    $mail->Host = "smtp.umbler.com";
-    $mail->Port = 587;
+        $mail->IsSMTP();
+        $mail->Host = "smtp.umbler.com";
+        $mail->Port = 587;
 
-    $mail->SMTPAuth = true;
-    $mail->Username = 'marcus@primeiro-imovel.com';
-    $mail->Password = 'cuzao123';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'marcus@primeiro-imovel.com';
+        $mail->Password = 'cuzao123';
 
 // Configurações de compatibilidade para autenticação em TLS
-    $mail->SMTPOptions = array(
-        'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => true,
-            'allow_self_signed' => true,
-        )
-    );
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => true,
+                'allow_self_signed' => true,
+            )
+        );
 
-    $mail->SMTPDebug = 2; // Você pode habilitar esta opção caso tenha problemas. Assim pode identificar mensagens de erro.
+       // $mail->SMTPDebug = 2; // Você pode habilitar esta opção caso tenha problemas. Assim pode identificar mensagens de erro.
 
-    $mail->From = "marcus@primeiro-imovel.com"; // Seu e-mail
-    $mail->FromName = "Marcus Lemos"; // Seu nome
+        $mail->From = "marcus@primeiro-imovel.com"; // Seu e-mail
+        $mail->FromName = "Marcus Lemos"; // Seu nome
 
 // Define o(s) destinatário(s)
-    $mail->AddAddress( $_SESSION['email'], $_SESSION['nome']);
+        $mail->AddAddress($_SESSION['email'], $_SESSION['nome']);
 
 // BCC - Cópia oculta
-    $mail->AddBCC('lipe.smo@gmail.com', 'Felipe');
+        $mail->AddBCC('lipe.smo@gmail.com', 'Felipe');
 
 // Definir se o e-mail é em formato HTML ou texto plano
-    $mail->IsHTML(true); // Formato HTML . Use "false" para enviar em formato texto simples.
+        $mail->IsHTML(true); // Formato HTML . Use "false" para enviar em formato texto simples.
 
-    $mail->CharSet = 'UTF-8'; // Charset (opcional)
+        $mail->CharSet = 'UTF-8'; // Charset (opcional)
 
 // Assunto da mensagem
-    $mail->Subject = "[Primeiro Imóvel] Seu primeiro imóvel, nosso primeiro passo!";
+        $mail->Subject = "[Primeiro Imóvel] Seu primeiro imóvel, nosso primeiro passo!";
 
 // Corpo do email
-    $mail->Body = '<b>SOLICITAÇÃO DE ORÇAMENTO VIA SITE</b>
-                    <br><br>
-                    <b>Nome: </b>';
+        $mail->Body = ' Olá, '.$_SESSION['nome'].'
+                        Estou entrando contato para dar continuidade a jornada de busca pelo seu imóvel dos sonhos!<br>
+                        Para te entregar o melhor resultado queria ver se você pode me ajudar respondendo duas perguntas:<br>
+                        Gostaria de entender a quanto tempo você está pesquisando por um imóvel?<br>
+                        E em quanto tempo você planeja realizar a comprar?<br><br>
+                        Fora isso, tem alguma coisa que você gostaria de perguntar ou que acha que deveriamos saber?<br>
+                        Queremos ser o mais assertivo possivel!<br><br>
+                        Abraços,<br>
+                        Marcus Lemos<br>
+                        Primeiro Imóvel';
 
 // Envia o e-mail
-    $enviado = $mail->Send();
+        $enviado = $mail->Send();
 
-// Exibe uma mensagem de resultado
-    if ($enviado) {
-        $retorno = array('codigo' => 1, 'mensagem' => '<br>Em breve entraremos em contato!');
-        echo $retorno;
-        exit();
-    } else {
-        $retorno = array('codigo' => 0, 'mensagem' => $mail->ErrorInfo);
-        echo $retorno;
-        exit();
-    }
 
         session_destroy();
 
         //var_dump($res);
-        //header('Location: obrigado.html');
+        header('Location: obrigado.html');
 
         exit();
     }
 
-        header('Location: step' .$pg. '.html');
+    header('Location: step' . $pg . '.html');
 
 }
